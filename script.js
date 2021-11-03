00df221aa48c078982d514dfe0c6dc3ad6f8e557
UD <dingding8003@gmail.com>
2021-11-03 01:47:27 -0500

    made some feedback changes

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function closeAccordionItem(Item) {
 }
 
 function toggleAccordionItem(Item) {
-  // Toggle functionality
   if (Item.classList.contains("active")) {
     Item.classList.remove("active");
     Item.nextElementSibling.style.maxHeight = 0;
@@ -21,17 +20,14 @@ function toggleAccordionItem(Item) {
 
 accordionItemHeaders.forEach((Header) => {
   Header.addEventListener("click", () => {
-    // toggle clicked header
     if (Header.classList.contains("active")) {
       toggleAccordionItem(Header);
     } else {
-      //close all open accordion items
       document
         .querySelectorAll(".accordion-item-header.active")
         .forEach((openHeader) => {
           closeAccordionItem(openHeader);
         });
-        // open individual header
       Header.classList.add("active");
       if (Header.classList.contains("active")) {
         Header.nextElementSibling.style.maxHeight =
